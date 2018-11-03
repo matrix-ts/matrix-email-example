5ca37bcdb75524ec8c998fabdae8285cce957115
Tilman <matrix@tilmans.space>
2018-11-03 13:24:53 +0100

    Add spark post

--- server.js
+++ server.js
@@ -3,16 +3,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var SparkPost = require("sparkpost");
+var sparky = new SparkPost();
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/matrix", (req, res) => {
-  // Do something with the request
-  res.send("Done");
+	sparky.transmissions
+		.send({
+			options: {
+				sandbox: true
+			},
+			content: {
+				from: "testing@" + process.env.SPARKPOST_SANDBOX_DOMAIN, // 'testing@sparkpostbox.com'
+				subject: "A change happened!",
+				html:
+					"<html><body><p>Testing SparkPost - the world's most awesomest email service!</p></body></html>"
+			},
+			recipients: [{ address: "developers+nodejs@sparkpost.com" }]
+		})
+		.then(data => {
+			console.log("Woohoo! You just sent your first mailing!");
+			console.log(data);
+		})
+		.catch(err => {
+			console.log("Whoops! Something went wrong");
+			console.log(err);
+		});
+	res.send("Done");
 });
 
 var server = app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log("Server now running on port", port);
+	var port = server.address().port;
+	console.log("Server now running on port", port);
 });
